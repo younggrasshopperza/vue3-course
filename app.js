8a33ead87b9fe9369b969c1f9a721f16e420b512
Alex Sexwale <alex@lcstudio.co.za>
2021-11-17 18:54:16 +0200

    Tutorial-1

---
+++ app.js
@@ -0,0 +1,6 @@
+const app = Vue.createApp({
+    // uncomment to see the output in the browser
+    template: '<h2>I am the template you are seeing  after uncommenting the code</h2>'
+})
+
+app.mount('#app');
