71b8f9745ef64fa68b1928d40e458d94a5f1b825
Alex Sexwale <alexsexwale@gmail.com>
2021-11-23 10:59:09 +0200

    click event

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const app = Vue.createApp({
-    // short hand for a function
-    data() {
-        // We are returning an object inside the function
-      return {
-        name: 'Xolani',
-        verb: ' is',
-        sentence: ' living his best lives at',
-        age: '25'
-      }
+  // short hand for a function
+  data() {
+      // We are returning an object inside the function
+    return {
+      name: 'Xolani',
+      sentence: ' is living his best lives at',
+      age: 25
     }
+  },
+  methods: {
+      changeName() {
+        // putting the word 'this' infront of the data value references the component itself   
+        this.name = "Palesa";
+      },
+      // can access the value passed through the paramenter
+      changeSentence(sentence) {
+          this.sentence = sentence;
+      }
+  }
 })
 
 app.mount('#app');
