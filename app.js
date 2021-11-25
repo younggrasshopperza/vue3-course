6d5895e6c1d7f1fdf9b1731203683f3c7ca96ebe
Alex Sexwale <alexsexwale@gmail.com>
2021-11-26 01:05:16 +0200

    conditional rendering

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const app = Vue.createApp({
-    // uncomment to see the output in the browser
-    template: '<h2>I am the template you are seeing  after uncommenting the code</h2>'
+  // short hand for a function
+  data() {
+      // We are returning an object inside the function
+    return {
+      showSentence: true,
+      name: 'Xolani',
+      sentence: ' is living his best lives at',
+      age: 25
+    }
+  },
+  methods: {
+      toggleShowSentence() {
+        // ! is used to reverse the value to what it is not: switching between true and false
+        this.showSentence = !this.showSentence
+      }
+  }
 })
 
 app.mount('#app');
