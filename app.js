980c329a7c5b842e5e3e24b3cec33d0771fdad00
Alex Sexwale <alexsexwale@gmail.com>
2021-11-26 07:35:59 +0200

    dynamic classes

--- app.js
+++ app.js
@@ -10,19 +10,22 @@ const app = Vue.createApp({
           artist: 'Soa Mattrix',
           album: 'uThando',
           genre: 'Amapiano',
-          img: 'assets/1.jpg'
+          img: 'assets/1.jpg',
+          myFav: true
         },
         {
           artist: 'Black Coffee',
           album: 'Pieces of Me',
           genre: 'House',
-          img: 'assets/2.jpg'
+          img: 'assets/2.jpg',
+          myFav: false
         },
         {
           artist: 'Sho Madjozi',
           album: 'Limpopo Champions League',
           genre: 'Gqom',
-          img: 'assets/3.jpg'
+          img: 'assets/3.jpg',
+          myFav: true
         }
       ]
     }
