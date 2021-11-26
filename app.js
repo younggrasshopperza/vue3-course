afd28a467f7f90963a8537aacdaec12f4faca479
Alex Sexwale <alexsexwale@gmail.com>
2021-11-26 07:13:18 +0200

    attribute binding

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ const app = Vue.createApp({
   data() {
       // We are returning an object inside the function
     return {
+      url: "https://www.younggrasshopper.co.za",
       showDiscography: true,
       discographies: [
         {
           artist: 'Soa Mattrix',
           album: 'uThando',
-          genre: 'Amapiano'
+          genre: 'Amapiano',
+          img: 'assets/1.jpg'
         },
         {
           artist: 'Black Coffee',
           album: 'Pieces of Me',
-          genre: 'House'
+          genre: 'House',
+          img: 'assets/2.jpg'
         },
         {
           artist: 'Sho Madjozi',
           album: 'Limpopo Champions League',
-          genre: 'Gqom'
+          genre: 'Gqom',
+          img: 'assets/3.jpg'
         }
       ]
     }
