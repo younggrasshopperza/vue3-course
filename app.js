de1cca75a047ae497d1b9c929353b1ef4e500e17
Alex Sexwale <alexsexwale@gmail.com>
2021-11-26 02:25:24 +0200

    looping using v-for

--- app.js
+++ app.js
@@ -3,32 +3,30 @@ const app = Vue.createApp({
   data() {
       // We are returning an object inside the function
     return {
-      showSentence: true,
-      name: 'Xolani',
-      sentence: ' is living his best lives at',
-      age: 25,
-      x: 0,
-      y: 0
+      showDiscography: true,
+      discographies: [
+        {
+          artist: 'Soa Mattrix',
+          album: 'uThando',
+          genre: 'Amapiano'
+        },
+        {
+          artist: 'Black Coffee',
+          album: 'Pieces of Me',
+          genre: 'House'
+        },
+        {
+          artist: 'Sho Madjozi',
+          album: 'Limpopo Champions League',
+          genre: 'Gqom'
+        }
+      ]
     }
   },
   methods: {
-      toggleShowSentence() {
-        // ! is used to reverse the value to what it is not: switching between true and false
-        this.showSentence = !this.showSentence
-      },
-      // inserting a value in the first parameter will allow you to view the event object 
-      handleEvent(e, number) {
-        // open the console on the web browser to view the event
-        console.log(e, e.type);
-        //will only work if we are passing an arguement in the parameter
-        if(number) {
-          console.log(number);
-        }
-      },
-      handleMouseMove(e) {
-        this.x = e.offsetX;
-        this.y = e.offsetY;
-      }
+    toggleShowDiscography() {
+      this.showDiscography = !this.showDiscography;
+    }
   }
 })
 
