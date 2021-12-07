45b9c8fce355e9a56b9020bc5afb317965105f82
Alex Sexwale <alexsexwale@gmail.com>
2021-12-08 00:09:05 +0200

    mouse events

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const app = Vue.createApp({
-  // short hand for a function
-  data() {
-      // We are returning an object inside the function
-    return {
-      showDiscography: true,
-      discographies: [
-        {
-          artist: 'Soa Mattrix',
-          album: 'uThando',
-          genre: 'Amapiano'
+    // short hand for a function
+    data() {
+        // We are returning an object inside the function
+      return {
+        showSentence: true,
+        name: 'Xolani',
+        sentence: ' is living his best lives at',
+        age: 25,
+        x: 0,
+        y: 0
+      }
+    },
+    methods: {
+        toggleShowSentence() {
+          // ! is used to reverse the value to what it is not: switching between true and false
+          this.showSentence = !this.showSentence
         },
-        {
-          artist: 'Black Coffee',
-          album: 'Pieces of Me',
-          genre: 'House'
+        // inserting a value in the first parameter will allow you to view the event object 
+        handleEvent(e, number) {
+          // open the console on the web browser to view the event
+          console.log(e, e.type);
+          //will only work if we are passing an arguement in the parameter
+          if(number) {
+            console.log(number);
+          }
         },
-        {
-          artist: 'Sho Madjozi',
-          album: 'Limpopo Champions League',
-          genre: 'Gqom'
+        handleMouseMove(e) {
+          this.x = e.offsetX;
+          this.y = e.offsetY;
         }
-      ]
     }
-  },
-  methods: {
-    toggleShowDiscography() {
-      this.showDiscography = !this.showDiscography;
-    }
-  }
-})
-
-app.mount('#app');
+  })
+  
+  app.mount('#app');
