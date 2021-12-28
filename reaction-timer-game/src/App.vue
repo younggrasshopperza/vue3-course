f7300a3361e386620b4c235381a17f636d80aaee
Alex Sexwale <alexsexwale@gmail.com>
2021-12-28 11:15:26 +0200

    Start Game

--- reaction-timer-game/src/App.vue
+++ reaction-timer-game/src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <h1>Young Grasshopper Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-
+import Block from './components/Block.vue';
 export default {
   name: 'App',
   components: {
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      // Random number between 2000 and 7000 milliseconds
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    }
   }
 }
 </script>
