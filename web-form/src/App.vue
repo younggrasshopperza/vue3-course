9e56a2f9ed486425781c8d210e648a8873378940
Alex Sexwale <alexsexwale@gmail.com>
2022-01-06 11:34:03 +0200

    Forms Intro & Setup

---
+++ web-form/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ <Signup />
+</template>
+
+<script>
+import Signup from "./components/Signup.vue";
+export default {
+  name: 'App',
+  components: {
+    Signup
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
+
