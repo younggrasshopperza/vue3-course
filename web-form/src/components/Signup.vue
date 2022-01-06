44d16ae685e6b2aa40870d3049cbcbc000c4a204
Alex Sexwale <alexsexwale@gmail.com>
2022-01-06 22:52:29 +0200

    Checkboxes

--- web-form/src/components/Signup.vue
+++ web-form/src/components/Signup.vue
@@ -1,23 +1,43 @@
 <template>
   <form>
       <label>Email:</label>
-      <input type="email" v-model="email">
+      <input type="email" v-model="email" required>
 
       <label>Password:</label>
-      <input type="password" v-model="password">
+      <input type="password" v-model="password" required>
 
       <label>Role:</label>
-      <select v-model="role">
+      <select v-model="role" required>
           <option value="Tech Lead">Tech Lead</option>
           <option value="Developer">Developer</option>
           <option value="Business Analyst">Business Analyst</option>
           <option value="Project Manager">Project Manager</option>
       </select>
+
+      <div class="terms">
+          <input type="checkbox" v-model="terms" required>
+          <label>Accept Terms and Conditions</label>
+      </div>
+
+      <div>
+          <input type="checkbox" value="Xolani" v-model="names">
+          <label>Xolani</label>
+      </div>
+      <div>
+          <input type="checkbox" value="Palesa" v-model="names">
+          <label>Palesa</label>
+      </div>
+      <div>
+          <input type="checkbox" value="Brandon" v-model="names">
+          <label>Brandon</label>
+      </div>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms and Conditions: {{ terms }}</p>
+  <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -26,7 +46,9 @@ export default {
         return {
             email: "",
             password: "",
-            role: ""
+            role: "",
+            terms: false,
+            names: []
         }
     }
 }
@@ -58,5 +80,12 @@ input, select {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
 }
 </style>
