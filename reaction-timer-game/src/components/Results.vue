d98954426d1cd783f20060a5cd81fb47cca70a29
Alex Sexwale <alexsexwale@gmail.com>
2022-01-06 10:22:07 +0200

    Reaction Quickness

--- reaction-timer-game/src/components/Results.vue
+++ reaction-timer-game/src/components/Results.vue
@@ -1,14 +1,34 @@
 <template>
   <p>Reaction time - {{ score }}</p>
+  <p class="speed">{{ speed }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
-
+    props: ['score'],
+    data() {
+        return {
+            speed: null
+        }
+    },
+    mounted() {
+      if(this.score < 250) {
+          this.speed = "Very fast"
+      }
+      else if (this.score < 400 ) {
+        this.speed = "Relatively fast"  
+      }  
+      else {
+        this.speed = "Not so fast"
+      }
+    }
 }
 </script>
 
 <style>
-
+  .speed {
+      font-size: 1.4em;
+      color: #0faf87;
+      font-weight: bold;
+  }
 </style>
