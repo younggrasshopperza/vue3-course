a676e823f3d5fe48de380f31845e599f14c26a9b
Alex Sexwale <alexsexwale@gmail.com>
2022-01-11 05:23:09 +0200

    Folder Structure

---
+++ young-grasshopper-jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+      <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                { id: 1, title: 'Software Developer', details: 'loerm' },
+                { id: 2, title: 'Web Designer', details: 'loerm' },
+                { id: 3, title: 'Project Manager', details: 'loerm' }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
